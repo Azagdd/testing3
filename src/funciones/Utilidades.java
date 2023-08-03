8e7533156967fae7f93b460921db7fff8587aec5
davidcasasdev <davidecasas@yandex.ru>
2023-08-03 08:38:43 +0200

    completo

--- src/funciones/Utilidades.java
+++ src/funciones/Utilidades.java
@@ -1,7 +1,18 @@
 package funciones;
 
+/** 
+ * Clase de utilidades
+ * @author David
+ *
+ */
 public class Utilidades {
 
+	/**
+	 * Instancia un objeto de la clase
+	 */
+	public Utilidades() {
+	}
+	
 	/**
 	 * Constante que contiene el número de horas de una joranda normal
 	 * {@value}
@@ -13,14 +24,36 @@ public class Utilidades {
 	 * pasada como parámetro
 	 * @param nota double con la nota a comprobar
 	 * @return Una cadena de tipo String con el texto resultante (Suspenso, Bien,
-	 * Notable, Sobrasaliente o Matrícula)
+	 * Notable, Sobresaliente o Matrícula)
 	 * @throws ArithmeticException cuando la nota no está entre 0 y 10
 	 */
 	public String devuelveNota(double nota) {
-		return null;
+		if (nota <0 || nota >10) {
+			throw new ArithmeticException("La nota debe ser negativo");
+		} else if (nota>=0 && nota<5){
+			return "Suspenso";
+		} else if (nota>=5 && nota<6.5) {
+			return "Bien";
+		} else if (nota<8.5) {
+			return "Notable";
+		} else if (nota<10) {
+			return "Sobresaliente";
+		} else {
+			return "Matrícula";
+		}
 	}
 	
-	
+	/**
+	 * Método que calcula el salario de un empleado dado el numeros de horas que ha trabajado
+	 * el precio por hora y el precio de la horas extras.
+	 * Se pagarán hasta {@link HORASJORNADA} con el precioHoras, el resto se pagarán a precioExtras
+	 * @param numHoras numeros horas de jornada laboral del empleado
+	 * @param precioHoras precio al que cobra la hora ordinaria
+	 * @param precioExtras precio al que cobra las horas extras
+	 * @return double con el valor del salario completo
+	 * @throws ArithmeticException se lanza cuando el numero de horas o los precio son negativos o 
+	 * cero
+	 */
 	public double calculaSalario(int numHoras, double precioHoras, double precioExtras) {
 		int extras=0;
 		int horas=numHoras;
@@ -35,6 +68,12 @@ public class Utilidades {
 	}
 	
 
+	/**
+	 * Método que devuelve el numero de divisores que tiene el número pasado como parámetro
+	 * 
+	 * @param num número a comprobar
+	 * @return el número de divisores 
+	 */
 	public int cuentaDivisores(int num) {
 		int cuenta=0;
 		
@@ -53,9 +92,21 @@ public class Utilidades {
 	 * @return int con el menor número del vector.
 	 */
 	public double devuelveMenor(int v[]) {
-		return 0;
+		
+		int menor=v[0];
+		for (int i = 0; i < v.length; i++) {
+			if(v[i]<menor) {
+				menor=v[i];
+			}
+		}
+		return menor;
 	}
 	
+	/**
+	 * Algoritmo que ordena un vector a través del método de la burbuja
+	 * @param v int [] vector a ordenar, que no será modificado
+	 * @return int [] con el vector ordenado
+	 */
 	public int [] burbuja(int[] v)
     {
       int auxiliar;
--- test/funciones/UtildadesTest.java
+++ test/funciones/UtildadesTest.java
@@ -1,14 +1,21 @@
 package funciones;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class UtildadesTest {
 
 private static Utilidades utils;
 	
-	
+	@BeforeAll
+	static void inicializar() {
+		utils = new Utilidades();
+	}
 
 	@Test
 	void testDevuelveNota() {
@@ -43,11 +50,32 @@ private static Utilidades utils;
 	@Test
 	void testCalculaSalario() {
 		
+		assertThrows(ArithmeticException.class, ()->utils.calculaSalario(0, 40, 12));
+		assertThrows(ArithmeticException.class, ()->utils.calculaSalario(30, 0, 12));
+		assertThrows(ArithmeticException.class, ()->utils.calculaSalario(38, 40, 0));
+		
+		assertThrows(ArithmeticException.class, ()->utils.calculaSalario(-8, 40, 12));
+		assertThrows(ArithmeticException.class, ()->utils.calculaSalario(30, -30, 12));
+		assertThrows(ArithmeticException.class, ()->utils.calculaSalario(38, 40, -20));
+		
+		assertEquals((Utilidades.HORASJORNADA-10)*12, 
+				utils.calculaSalario(Utilidades.HORASJORNADA-10, 12, 15));
+		assertEquals(Utilidades.HORASJORNADA*12+2*15, 
+				utils.calculaSalario(Utilidades.HORASJORNADA+2, 12, 15));
+		assertEquals(Utilidades.HORASJORNADA*12, 
+				utils.calculaSalario(Utilidades.HORASJORNADA, 12, 15));
+		
 	}
 
 	@Test
 	void testCuentaDivisores() {
-
+		assertAll(
+				()->assertEquals(2, utils.cuentaDivisores(7)),
+				()->assertEquals(1, utils.cuentaDivisores(1)),
+				()->assertEquals(4, utils.cuentaDivisores(8)),
+				()->assertEquals(0, utils.cuentaDivisores(0)),
+				()->assertEquals(0, utils.cuentaDivisores(-8))
+				);
 	}
 
 	@Test
@@ -57,11 +85,27 @@ private static Utilidades utils;
 		
 		int v2[] = { 2,3,-8,1,7 };
 		assertEquals(-8, utils.devuelveMenor(v2));
+		
 	}
 
 	@Test
 	void testBurbuja() {
-
+		int v[] = {2,7,4,9,1};
+		int o[] = {1,2,4,7,9};
+		
+		assertArrayEquals(o, utils.burbuja(v));
+		
+		int v2[] = { 1,3,7,9,15};
+		assertArrayEquals(v2, utils.burbuja(v2));
+		
+		int v3[] = { 9, 3, 9, 1, 3, 5};
+		int o3[] = {1,3,3,5,9,9};
+		assertArrayEquals(o3, utils.burbuja(v3));
+		
+		int v4[] = {9,7,5,3,1};
+		int o4[] = {1,3,5,7,9};
+		assertArrayEquals(o4, utils.burbuja(v4));
+		
 	}
 
 }
